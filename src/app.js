fce6ac4fd233a8e809a1a875115a928cacd03a9f
Diego Bisego <diegobisego@gmail.com>
2023-04-25 12:40:14 -0300

    se agrega la lista de handlebars

--- src/app.js
+++ src/app.js
@@ -1,6 +1,26 @@
 import express from "express";
 import morgan from "morgan"; //morgan para debug
+import __dirname from "./utils.js";
 const app = express();
+
+//handlebars
+import handlebars from "express-handlebars";
+app.engine("handlebars", handlebars.engine());
+app.set("views", `${__dirname}/views`);
+app.set("view engine", "handlebars");
+
+import ProductManager from "./manager/ProductManager.js";
+const newProductManager = new ProductManager("src/db/products.json")
+
+app.get("/", async (_req, res) => {
+
+    const resultProducts = await newProductManager.getProducts() 
+    const products = resultProducts.data
+
+  res.render("home", {
+    products
+  });
+});
 
 //middlewares
 app.use(express.json());
@@ -8,7 +28,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(morgan("dev"));
 
 //rutas
-import routes from './index.js'
-app.use('/', routes)
+import routes from "./index.js";
+app.use("/", routes);
 
 export default app;
